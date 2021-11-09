cc2edbb3fd093782de5d39d20836c2ecfcbf6848
DazhiZhong <zhongdz.dazhi2018@gdhfi.com>
2021-11-09 02:08:25 -0800

    added gaussian and edge_image /test

--- Losses/EdgeLoss.py
+++ Losses/EdgeLoss.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn, optim
-
+from PIL import Image
+from util import *
+from urllib.request import urlopen
+import torchvision.transforms.functional as TF
 
 
 
@@ -8,6 +11,8 @@ from Losses.LossInterface import LossInterface
 
 class EdgeLoss(LossInterface):
     def __init__(self,**kwargs):
+        self.image = None
+        self.resized = None
         super().__init__(**kwargs)
     
     @staticmethod
@@ -16,22 +21,38 @@ class EdgeLoss(LossInterface):
         parser.add_argument("--edge_color", nargs=3, type=float, help="this is the color of the specified region, in (R,G,B) 0-255", default=(255,255,255), dest='edge_color')
         parser.add_argument("--edge_color_weight", type=float, help="how much edge color is enforced", default=2, dest='edge_color_weight')
         parser.add_argument("--global_color_weight", type=float, help="how much global color is enforced ", default=0.5, dest='global_color_weight')
+        parser.add_argument("--edge_input_image", type=str, help="TBD", default="", dest='edge_input_image')
         return parser
 
     def parse_settings(self,args):
         #do stuff with args here
         if type(args.edge_color)==str:
             args.edge_color = (255,255,255)
+        if args.edge_input_image:
+            # now we might overlay an init image
+            filelist = None
+            if 'http' in args.edge_input_image:
+                self.image = [Image.open(urlopen(args.edge_input_image))]
+            else:
+                filelist = real_glob(args.edge_input_image)
+                self.image = [Image.open(f) for f in filelist]
+            self.image = self.image[0]
         return args
     
     def get_loss(self, cur_cutouts, out, args, globals=None, lossGlobals=None):
-        zers = torch.zeros(out.size()).cuda()
+        if self.resized is None:
+            self.resized = TF.to_tensor(self.image).to(self.device).unsqueeze(0)
+            self.resized = TF.resize(self.resized, out.size()[2:4],TF.InterpolationMode.BICUBIC)
+        if not args.edge_input_image:
+            zers = torch.zeros(out.size()).to(self.device)
+            Rval,Gval,Bval = args.edge_color
+            zers[:,0,:,:] = Rval/255
+            zers[:,1,:,:] = Gval/255
+            zers[:,2,:,:] = Bval/255
+        else: # args.edge_input_image:
+            zers = self.resized
         lmax = out.size()[2]
         rmax = out.size()[3]
-        Rval,Gval,Bval = args.edge_color
-        zers[:,0,:,:] = Rval/255
-        zers[:,1,:,:] = Gval/255
-        zers[:,2,:,:] = Bval/255
         mseloss = nn.MSELoss()
         left, right, upper, lower = args.edge_thickness
         cur_loss = torch.tensor(0.0).cuda()
---
+++ Losses/GaussianLoss.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn, optim
+from Losses.LossInterface import LossInterface
+
+
+def gaussian_fn(M, std):
+    n = torch.arange(0, M) - (M - 1.0) / 2.0
+    sig2 = 2 * std * std
+    w = torch.exp(-n ** 2 / sig2)
+    return w
+
+def gkern(ylen=256, xlen=256, stdy=128, stdx=128):
+    """Returns a 2D Gaussian kernel array."""
+    gkerny = gaussian_fn(ylen, std=stdy) 
+    gkernx = gaussian_fn(xlen, std=stdx) 
+    gkern2d = torch.outer(gkerny, gkernx)
+    return gkern2d
+
+class GaussianLoss(LossInterface):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+    
+    @staticmethod
+    def add_settings(parser):
+        parser.add_argument("--gaussian_weight", type=float, help="gaussian's weight", default=1, dest='gaussian_weight')
+        parser.add_argument("--gaussian_std", nargs=2 ,type=float, help="gaussian's std for both x and y", default=(40,40), dest='gaussian_std')
+        parser.add_argument("--gaussian_color", nargs=3 ,type=float, help="color for gaussian to optimize to", default=(255,255,255), dest='gaussian_color')
+        return parser
+    
+
+    def get_loss(self, cur_cutouts, out, args, globals=None, lossGlobals=None):
+        gaus = gkern(out.size()[2],out.size()[3], *args.gaussian_std).to(self.device)
+        color = torch.zeros(out.size()).to(self.device)
+        Rval,Gval,Bval = args.gaussian_color
+        color[:,0,:,:] = Rval/255
+        color[:,1,:,:] = Gval/255
+        color[:,2,:,:] = Bval/255
+        # mseloss = nn.MSELoss()
+        loss = torch.abs(out - color)
+        # print(loss.size()) 
+        loss = loss*torch.abs(1-gaus)
+
+        cur_loss = torch.mean(loss)
+        return cur_loss*args.gaussian_weight
