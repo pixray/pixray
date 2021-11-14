c2976c8863572a294b16bf8bde33cbd729ac9edf
Tom White <tom@sixdozen.com>
2021-11-14 01:49:36 +0000

    added mask image to edgeloss

--- Losses/EdgeLoss.py
+++ Losses/EdgeLoss.py
@@ -14,6 +14,7 @@ class EdgeLoss(LossInterface):
     def __init__(self,**kwargs):
         self.image = None
         self.resized = None
+        self.resized_mask = None
         super().__init__(**kwargs)
     
     @staticmethod
@@ -24,6 +25,7 @@ class EdgeLoss(LossInterface):
         parser.add_argument("--edge_color_weight", type=float, help="how much edge color is enforced", default=2, dest='edge_color_weight')
         parser.add_argument("--global_color_weight", type=float, help="how much global color is enforced ", default=0.5, dest='global_color_weight')
         parser.add_argument("--edge_input_image", type=str, help="TBD", default="", dest='edge_input_image')
+        parser.add_argument("--edge_mask_image", type=str, help="TBD", default="", dest='edge_mask_image')
         return parser
 
     def parse_settings(self,args):
@@ -42,14 +44,28 @@ class EdgeLoss(LossInterface):
                 filelist = real_glob(args.edge_input_image)
                 self.image = [Image.open(f) for f in filelist]
             self.image = self.image[0].convert('RGB')
+        if args.edge_mask_image:
+            # now we might overlay an init image
+            filelist = None
+            if 'http' in args.edge_mask_image:
+                self.mask = [Image.open(urlopen(args.edge_mask_image))]
+            else:
+                filelist = real_glob(args.edge_mask_image)
+                self.mask = [Image.open(f) for f in filelist]
+            self.mask = self.mask[0].convert('L')
         else:
-            self.image = None
+            self.mask = None
         return args
     
     def get_loss(self, cur_cutouts, out, args, globals=None, lossGlobals=None):
         if self.resized is None and self.image is not None:
             self.resized = TF.to_tensor(self.image).to(self.device).unsqueeze(0)
             self.resized = TF.resize(self.resized, out.size()[2:4],TF.InterpolationMode.BICUBIC)
+            print("IMAGE MASK: ", self.resized.shape)
+        if self.resized_mask is None and self.mask is not None:
+            self.resized_mask = TF.to_tensor(self.mask).to(self.device).unsqueeze(0)
+            self.resized_mask = TF.resize(self.resized_mask, out.size()[2:4],TF.InterpolationMode.BICUBIC)
+            print("RESIZED MASK: ", self.resized_mask.shape)
         if not self.image:
             zers = torch.zeros(out.size()).to(self.device)
             Rval,Gval,Bval = args.edge_color
@@ -58,23 +74,28 @@ class EdgeLoss(LossInterface):
             zers[:,2,:,:] = Bval
         else: # args.edge_input_image:
             zers = self.resized
-        lmax = out.size()[2]
-        rmax = out.size()[3]
-        mseloss = nn.MSELoss()
-        left, right, upper, lower = args.edge_margins
         cur_loss = torch.tensor(0.0).cuda()
-        lloss = mseloss(out[:,:,:,:left], zers[:,:,:,:left]) 
-        rloss = mseloss(out[:,:,:,rmax-right:], zers[:,:,:,rmax-right:])
-        uloss = mseloss(out[:,:,:upper,left:rmax-right], zers[:,:,:upper,left:rmax-right]) 
-        dloss = mseloss(out[:,:,lmax-lower:,left:rmax-right], zers[:,:,lmax-lower:,left:rmax-right]) 
-        if left!=0:
-            cur_loss+=lloss
-        if right!=0:
-            cur_loss+=rloss
-        if upper!=0:
-            cur_loss+=uloss
-        if lower!=0:
-            cur_loss+=dloss
+        mseloss = nn.MSELoss()
+        if not self.mask:
+            lmax = out.size()[2]
+            rmax = out.size()[3]
+            left, right, upper, lower = args.edge_margins
+            lloss = mseloss(out[:,:,:,:left], zers[:,:,:,:left]) 
+            rloss = mseloss(out[:,:,:,rmax-right:], zers[:,:,:,rmax-right:])
+            uloss = mseloss(out[:,:,:upper,left:rmax-right], zers[:,:,:upper,left:rmax-right]) 
+            dloss = mseloss(out[:,:,lmax-lower:,left:rmax-right], zers[:,:,lmax-lower:,left:rmax-right]) 
+            if left!=0:
+                cur_loss += lloss
+            if right!=0:
+                cur_loss += rloss
+            if upper!=0:
+                cur_loss += uloss
+            if lower!=0:
+                cur_loss += dloss
+        else:
+            maked_out = torch.where(self.resized_mask > 0, zers, out)
+            mask_loss = mseloss(maked_out, zers)
+            cur_loss += mask_loss
         if args.global_color_weight:
             gloss = mseloss(out[:,:,:,:], zers[:,:,:,:]) * args.global_color_weight
             cur_loss+=gloss
