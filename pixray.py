4c7c61784675ba25f724e0f7478b514fa3230c13
Tom White <tom@sixdozen.com>
2022-05-24 14:29:58 +0000

    floating version

--- pixray.py
+++ pixray.py
@@ -1109,7 +1109,7 @@ def checkdrop(args, iter, losses):
     return drop_loss_time
 
 # for a release just bake in the version to prevent git subprocess lookup
-git_official_release_version = "1.9.1"
+git_official_release_version = None
 git_fallback_version = "v1.9.1+"
 
 # https://stackoverflow.com/a/40170206/1010653
